5577b16d10f9b83d31423f2489ae0b1d1ccb7a58
mohammed3200 <wwyuu799@gmail.com>
2025-03-06 00:59:25 +0200

    Add credentials provider for user authentication with email and password

--- auth.ts
+++ auth.ts
@@ -5,7 +5,12 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
  
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const { 
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut
+ } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
