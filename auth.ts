f4cbf0b66523836200019c0fe7645d094c595484
mohammed3200 <wwyuu799@gmail.com>
2025-04-11 18:59:29 +0200

    Refactor authentication handlers and improve session and JWT callbacks for user role management

--- auth.ts
+++ auth.ts
@@ -1,17 +1,42 @@
 import NextAuth from "next-auth";
-
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
- 
-export const { 
-  handlers: { GET, POST },
+import { getUserById } from "@/data/user";
+
+export const {
+  handlers,
   auth,
   signIn,
-  signOut
- } = NextAuth({
-  adapter: PrismaAdapter(db),
+  signOut,
+} = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+
+    async jwt({ token, user, profile }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db), 
   session: { strategy: "jwt" },
   ...authConfig,
 });
