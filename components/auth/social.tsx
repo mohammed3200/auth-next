7007b282a4b447f85c5a1f371ce89e693520b1f6
mohammed3200 <wwyuu799@gmail.com>
2025-04-11 18:59:10 +0200

    Implement social login buttons for Google and GitHub with sign-in functionality

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,29 +1,36 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
-    return (
-        <div className="flex items-center w-full gap-x-2">
-            <Button 
-            size="lg"
-            className="basis-1/2"
-            variant="outline"
-            onClick={() => {}}
-            >
-                <FcGoogle className="h-5 w-5" />
-            </Button>
-            <Button 
-            size="lg"
-            className="basis-1/2"
-            variant="outline"
-            onClick={() => {}}
-            >
-                <FaGithub className="h-5 w-5" />
-            </Button>
-        </div>
-    );
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button
+        size="lg"
+        className="basis-1/2"
+        variant="outline"
+        onClick={() => onClick("google")}
+      >
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+      <Button
+        size="lg"
+        className="basis-1/2"
+        variant="outline"
+        onClick={() => onClick("github")}
+      >
+        <FaGithub className="h-5 w-5" />
+      </Button>
+    </div>
+  );
 };
