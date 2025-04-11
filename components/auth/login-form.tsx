c3c5c05d0a5824a7eafc9f002f7c0e13faa5344d
mohammed3200 <wwyuu799@gmail.com>
2025-04-11 20:54:45 +0200

    Add error handling for OAuth account linking in LoginForm

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import z from "zod";
-import { useTransition, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useSearchParams } from "next/navigation";
+import { useTransition, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
@@ -24,6 +25,10 @@ import { FormSuccess } from "@/components/form-success";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError = searchParams.get("error") === "OAuthAccountNotLinked" 
+  ? "Email already exists. Please login with different provider!"
+  : "";
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -42,8 +47,9 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // TODO: Add when we add 2FA
+        // setSuccess(data?.success);
       });
     });
   };
@@ -95,7 +101,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button disabled={isPending} type="submit" className="w-full">
             Login
