1a2cdaf17244c844b14f2ccab8c7f3d1c2498c5b
mohammed3200 <wwyuu799@gmail.com>
2025-04-13 16:29:13 +0200

    Add sendPasswordResetEmail function to handle password reset email sending

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,17 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+    const resetLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/reset-password?token=${token}`;
+
+    await resend.emails.send({
+        from: process.env.RESEND_EMAIL!,
+        to: email,
+        subject: "Reset your password",
+        html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`
+    });
+}
+
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/new-verification?token=${token}`;
 
