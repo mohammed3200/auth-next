acfb9cbdc1c518c6c6beb2720ea22c5325dd47eb
mohammed3200 <wwyuu799@gmail.com>
2025-04-13 17:03:29 +0200

    Update password reset link to point to new-password route

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-    const resetLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/reset-password?token=${token}`;
+    const resetLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/new-password?token=${token}`;
 
     await resend.emails.send({
         from: process.env.RESEND_EMAIL!,
