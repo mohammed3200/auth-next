642c9bb07ecd3e62bd91b5827d77f7bb9e8870d2
mohammed3200 <wwyuu799@gmail.com>
2025-04-13 19:30:39 +0200

    Add function to send two-factor authentication token email

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,15 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export const sendTwoFactorTokenEmail = async (email:string, token:string) => {
+    await resend.emails.send({
+        from: process.env.RESEND_EMAIL!,
+        to: email,
+        subject: "Your 2FA token",
+        html: `<p>Your 2FA token is <strong>${token}</strong></p>`
+    });
+}
+
 export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/new-password?token=${token}`;
 
