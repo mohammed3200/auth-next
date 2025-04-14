d881961d0c67795bfa132a422fc8c3b0cc5dc45d
mohammed3200 <wwyuu799@gmail.com>
2025-04-14 23:58:55 +0200

    feat: update LoginButton to support modal rendering and enhance LoginForm with callback URL handling

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -28,6 +28,7 @@ import { login } from "@/actions/login";
 
 export const LoginForm = () => {
   const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
   const urlError =
     searchParams.get("error") === "OAuthAccountNotLinked"
       ? "Email already exists. Please login with different provider!"
@@ -50,7 +51,7 @@ export const LoginForm = () => {
     setSuccess("");
 
     startTransition(() => {
-      login(values)
+      login(values, callbackUrl)
         .then((data) => {
           if (data?.error) {
             form.reset();
