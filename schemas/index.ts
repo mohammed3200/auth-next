fffe0679a8939883de22ebaed1d5a682a85c34aa
mohammed3200 <wwyuu799@gmail.com>
2025-04-14 23:01:11 +0200

    feat: add SettingsSchema for user settings validation with role and password requirements

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,30 @@
 import z from "zod";
+import { UserRole } from "@prisma/client";
+
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  .refine((data) => {
+    if (data.password && !data.newPassword) return false;
+
+    return true;
+  }, {
+    message: "New password is required!",
+    path: ["newPassword"],
+  }).refine((data) => {
+    if (data.newPassword && !data.password) return false;
+
+    return true
+  }, {
+    message: "Password is required!",
+    path: ["password"]
+  });
 
 export const LoginSchema = z.object({
   email: z.string().email({
@@ -28,9 +54,8 @@ export const ResetSchema = z.object({
   }),
 });
 
-export const NewPasswordSchema = z
-  .object({
-    password: z.string().min(6, {
-        message: "Minimum 6 characters required",
-    }),
-  });
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+});
